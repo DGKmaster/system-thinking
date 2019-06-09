92a254aab84d5e2074c536eff4318c818a695419
Petr <cf-a19@ya.ru>
2019-06-09 14:10:48 +0300

    failinterfaceApp
    Не работающий билд только с частью интерфейса.

---
+++ Android/app/src/main/java/com/hackaton/turistsapp3/RouteCreateActivity.java
@@ -0,0 +1,20 @@
+package com.hackaton.turistsapp3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RouteCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_create);
+    }
+
+    public void createRoute(View view) {
+        Intent intent = new Intent(this, RouteCreateActivity.class);
+        startActivity(intent);
+    }
+}
